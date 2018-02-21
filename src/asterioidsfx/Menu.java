49352c63bf681444ae1bfd203685d2c9f6ec3c1c
Wazbat <Wazbat@169.254.191.62>
2018-02-21 11:23:57 +0100

    Conversion a clases

---
+++ src/asterioidsfx/Menu.java
@@ -0,0 +1,14 @@
+
+package asterioidsfx;
+
+import javafx.scene.layout.Pane;
+
+public class Menu {
+    Pane rootMenu = new Pane();
+    
+    
+    
+    public Pane getRoot(){
+        return rootMenu;
+    };
+}
